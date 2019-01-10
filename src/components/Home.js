0841540e6638b0109980d233c979dc4b5b94abd1
Dvontre Coleman <dvontrec@gmail.com>
2019-01-09 20:55:18 -0500

    add axios request to fortnite api

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,6 @@ import Nav from './Nav';
 
 class Home extends Component {
   getData() {
-    let headers = {
-      'trn-Api-Key': 'a3a6cde3-be5c-4a74-9897-bae032c666e7'
-    };
     axios
       .get(
         'https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=BossHeathen502'
